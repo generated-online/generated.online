3a36f9f83d368b8621182b56ebf3a9e0428a5b43
Sebastian Fellner <fellnerseb@gmail.com>
2019-10-02 15:39:01 +0200

    implement datahandler

--- food/src/preprocess/data_handling.py
+++ food/src/preprocess/data_handling.py
@@ -1,40 +1,50 @@
-import glob
 import json
-import os
 import pickle
 from json import JSONDecodeError
+from pathlib import Path
 from typing import List
 
 from tqdm import tqdm
 
-from preprocess.main import MODULE_NAME
 
+class DataHandler:
+    def __init__(self, data_folder: Path = None):
+        if not data_folder:
+            data_folder = Path(__file__).parent.parent / "data"
+        self.data_folder = data_folder
+        self.recipes_folder = data_folder / "recipes"
+        self.concatenated_file = data_folder / "all_recipes.pkl"
 
-def concatenate_data_files(
-    data_folder: str = "../chefkoch/data", output_file: str = "data.pkl"
-):
-    concatenated_list = []
+    def concatenate_data_files(self):
+        if not self.recipes_folder.exists():
+            raise FileNotFoundError(
+                "Folder with recipes not found. Did you ran the" "crawler?"
+            )
+        concatenated_list_of_recipes = []
 
-    all_data_file_names = glob.glob(data_folder + "/*.json")
+        all_data_file_names = self.recipes_folder.glob("*.json")
 
-    for data_file_name in tqdm(all_data_file_names):
-        try:
-            with open(data_file_name, "r") as data_file:
-                data_list = json.load(data_file)
-                concatenated_list += data_list
-        except JSONDecodeError:
-            print(f"couldn't load file {data_file_name}")
+        for data_file_name in tqdm(all_data_file_names):
+            try:
+                with open(data_file_name, "r") as data_file:
+                    data_list = json.load(data_file)
+                    concatenated_list_of_recipes += data_list
+            except JSONDecodeError:
+                print(f"couldn't load file {data_file_name}")
 
-    with open(output_file, "wb") as f:
-        pickle.dump(concatenated_list, f)
+        with open(self.concatenated_file, "wb") as f:
+            pickle.dump(concatenated_list_of_recipes, f)
 
+    def read_data(self, data_file: Path = None) -> List[dict]:
+        if not data_file:
+            data_file = self.concatenated_file
+        with open(data_file, "rb") as f:
+            return pickle.load(f)
 
-def read_data(data_file: str = "data.pkl") -> List[dict]:
-    with open(data_file, "rb") as f:
-        return pickle.load(f)
-
-
-def read_small_data() -> List[dict]:
-    filename = os.path.join(MODULE_NAME, "../chefkoch/data/data_000100.json")
-    with open(filename, "r") as f:
-        return json.load(f)
+    def read_small_data(self) -> List[dict]:
+        if not self.recipes_folder.exists():
+            raise FileNotFoundError(
+                "Folder with recipes not found. Did you ran the" "crawler?"
+            )
+        with open(self.recipes_folder / "data_000100.json", "r") as f:
+            return json.load(f)
--- food/src/preprocess/main.py
+++ food/src/preprocess/main.py
@@ -1,146 +1,4 @@
-import os
-import sys
-from typing import List
-
-import numpy as np
-from matplotlib import pyplot as plt
-
-from preprocess.data_handling import data
-from preprocess.data_handling import read_small_data
-
-MODULE_NAME = os.path.dirname(__file__)
-
-
-def filter_none_ratings(data: List[dict]) -> List[dict]:
-    """Remove data points that contain no rating."""
-
-    filtered_data = list(filter(lambda x: x["rating"], data))
-    recipes_lost = len(data) - len(filtered_data)
-    print(
-        f"Lost {recipes_lost} data points! ({recipes_lost/len(data)*100}%)",
-        file=sys.stderr,
-    )
-    return filtered_data
-
-
-def draw_hist(data, title: str, xlabel: str, ylabel: str, log: bool, *args, **kwargs):
-    plt.hist(data, log=log, bins=100, *args, **kwargs)
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.show()
-
-
-def draw_hist_views(data=None, log: bool = True):
-    if data is None:
-        data = read_small_data()
-
-    views = list(map(lambda x: x["text"]["viewCount"], data))
-
-    draw_hist(
-        views,
-        f"Chefkoch-recipes views histogram{' (logarithmic scale)' if log else ''}",
-        "Number of recipes",
-        "number of views",
-        log=log,
-    )
-
-
-def draw_hist_ratings(data=None, log: bool = True):
-    if data is None:
-        data = filter_none_ratings(read_small_data())
-
-    ratings = list(map(lambda x: x["rating"]["rating"] if x["rating"] else -1, data))
-
-    draw_hist(
-        ratings,
-        f"Chefkoch-recipes rating histogram{' (logarithmic scale)' if log else ''}",
-        "rating in stars",
-        "Number of recipes",
-        log=log,
-    )
-
-
-def draw_hist_views_cummulative(data=None):
-    if data is None:
-        data = read_small_data()
-
-    views = list(map(lambda x: x["text"]["viewCount"], data))
-
-    """
-    1. sort data:
-        0 0 0 1 2 2 1000
-        0 1 2 3 4 5    6
-    2. cummulative
-        0 0 0 1 3 5 1005
-        0 1 2 3 4 5    6
-    3. devide by max
-    """
-
-    views.sort()
-    views = np.asarray(views, dtype=np.float)
-    views /= views[-1]
-    views = 1 - views
-    views = np.flip(views) * 100
-
-    plt.plot(np.linspace(0, 100, num=len(views)), views)
-    plt.title("Cumulative percentage of views vs % recipes")
-    plt.xlabel("% of recipes")
-    plt.ylabel("% of views")
-    plt.show()
-
-
-def draw_views_vs_ratings(data=None):
-    if data is None:
-        data = read_small_data()
-
-    views = list(map(lambda x: x["text"]["viewCount"], data))
-    ratings = list(map(lambda x: x["rating"]["rating"] if x["rating"] else -1, data))
-    sorted_views_and_ratings = sorted(zip(views, ratings), key=lambda x: x[0])
-    views, ratings = zip(*sorted_views_and_ratings)
-    plt.plot(views, ratings)
-
-    plt.title("Views vs Rating")
-    plt.xlabel("Number of Views")
-    plt.ylabel("Rating")
-    plt.show()
-
-
-def draw_number_of_votes_vs_ratings(data=None):
-    if data is None:
-        data = read_small_data()
-
-    number_of_votes = list(
-        map(lambda x: x["rating"]["numVotes"] if x["rating"] else -1, data)
-    )
-    ratings = list(map(lambda x: x["rating"]["rating"] if x["rating"] else -1, data))
-    sorted_views_and_ratings = sorted(zip(number_of_votes, ratings), key=lambda x: x[0])
-    views, ratings = zip(*sorted_views_and_ratings)
-    plt.plot(views, ratings)
-
-    plt.title("Number of Votes vs Rating")
-    plt.xlabel("Number of Votes")
-    plt.ylabel("Rating")
-    plt.show()
-
-
-def draw_number_of_votes_vs_views(data=None):
-    if data is None:
-        data = read_small_data()
-
-    number_of_votes = list(
-        map(lambda x: x["rating"]["numVotes"] if x["rating"] else -1, data)
-    )
-    views = list(map(lambda x: x["text"]["viewCount"], data))
-    sorted_views_and_ratings = sorted(zip(views, number_of_votes), key=lambda x: x[0])
-    views, ratings = zip(*sorted_views_and_ratings)
-    plt.plot(views, ratings)
-
-    plt.title("Views vs Rating")
-    plt.xlabel("Views")
-    plt.ylabel("number_of_votes")
-    plt.show()
-
+from preprocess.data_handling import DataHandler
 
 if __name__ == "__main__":
     # data = read_small_data()
@@ -151,6 +9,11 @@ if __name__ == "__main__":
     #
     # draw_hist_views_cummulative(data=data)
 
-    draw_views_vs_ratings(data=filter_none_ratings(data))
-    draw_number_of_votes_vs_ratings(data=filter_none_ratings(data))
-    draw_number_of_votes_vs_views(data=filter_none_ratings(data))
+    # draw_views_vs_ratings(data=filter_none_ratings(data))
+    # draw_number_of_votes_vs_ratings(data=filter_none_ratings(data))
+    # draw_number_of_votes_vs_views(data=filter_none_ratings(data))
+
+    d = DataHandler()
+    d.concatenate_data_files()
+    d.read_small_data()
+    d.read_data()
