df53822bce0ff0e62ce4b71eb346eb3a634fd086
Sebastian Fellner <fellnerseb@gmail.com>
2019-10-02 15:56:25 +0200

    small refactoring

--- food/src/preprocess/main.py
+++ food/src/preprocess/main.py
@@ -1,18 +1,6 @@
 from preprocess.data_handling import DataHandler
 
 if __name__ == "__main__":
-    # data = read_small_data()
-    #
-    # draw_hist_ratings(data=filter_none_ratings(data))
-    #
-    # draw_hist_views(data=data)
-    #
-    # draw_hist_views_cummulative(data=data)
-
-    # draw_views_vs_ratings(data=filter_none_ratings(data))
-    # draw_number_of_votes_vs_ratings(data=filter_none_ratings(data))
-    # draw_number_of_votes_vs_views(data=filter_none_ratings(data))
-
     d = DataHandler()
     d.concatenate_data_files()
     d.read_small_data()
