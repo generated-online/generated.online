bfc1693c94e785611632f5bf35fa1da3490f5ce7
Sebastian Fellner <fellnerseb@gmail.com>
2019-10-02 09:50:51 +0200

    add cumulative diagram

--- preprocess/main.py
+++ preprocess/main.py
@@ -6,6 +6,7 @@ import sys
 from json import JSONDecodeError
 from typing import List
 
+import numpy as np
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 
@@ -54,8 +55,8 @@ def filter_none_ratings(data: List[dict]) -> List[dict]:
     return filtered_data
 
 
-def draw_hist(log: bool, data, title: str, xlabel: str, ylabel: str):
-    plt.hist(data, log=log, bins=100)
+def draw_hist(data, title: str, xlabel: str, ylabel: str, log: bool, *args, **kwargs):
+    plt.hist(data, log=log, bins=100, *args, **kwargs)
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -69,11 +70,11 @@ def draw_hist_views(data=None, log: bool = True):
     views = list(map(lambda x: x["text"]["viewCount"], data))
 
     draw_hist(
-        log,
         views,
         f"Chefkoch-recipes views histogram{' (logarithmic scale)' if log else ''}",
-        "number of views",
         "Number of recipes",
+        "number of views",
+        log=log,
     )
 
 
@@ -84,17 +85,49 @@ def draw_hist_ratings(data=None, log: bool = True):
     ratings = list(map(lambda x: x["rating"]["rating"] if x["rating"] else -1, data))
 
     draw_hist(
-        log,
         ratings,
         f"Chefkoch-recipes rating histogram{' (logarithmic scale)' if log else ''}",
         "rating in stars",
         "Number of recipes",
+        log=log,
     )
 
 
-if __name__ == "__main__":
-    # draw_hist_ratings()
-    draw_hist_ratings(data=filter_none_ratings(read_data()))
+def draw_hist_views_cummulative(data=None):
+    if data is None:
+        data = read_small_data()
 
-    # draw_hist_views()
-    # draw_hist_views(data=read_data())
+    views = list(map(lambda x: x["text"]["viewCount"], data))
+
+    """
+    1. sort data:
+        0 0 0 1 2 2 1000
+        0 1 2 3 4 5    6
+    2. cummulative
+        0 0 0 1 3 5 1005
+        0 1 2 3 4 5    6
+    3. devide by max
+    """
+
+    views.sort()
+    views = np.asarray(views, dtype=np.float)
+    views /= views[-1]
+    views = 1 - views
+    views = np.flip(views) * 100
+
+    plt.plot(np.linspace(0, 100, num=len(views)), views)
+    plt.title("Cumulative percentage of views vs % recipes")
+    plt.xlabel("% of recipes")
+    plt.ylabel("% of views")
+    plt.show()
+
+
+if __name__ == "__main__":
+    data = read_data()
+    # data = read_small_data()
+
+    draw_hist_ratings(data=filter_none_ratings(data))
+
+    draw_hist_views(data=data)
+
+    draw_hist_views_cummulative(data=data)
