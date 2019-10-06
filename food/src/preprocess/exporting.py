9375ac261185fa9b633e802383ccb81953584299
Sebastian Fellner <fellnerseb@gmail.com>
2019-10-06 16:01:29 +0200

    add exporter

---
+++ food/src/preprocess/exporting.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Exporter:
+    def __init__(self, data: List[dict]):
+        self.data = data
+
+    def _reicpe_to_text(self, recipe_dict: dict):
+        recipe = ""
+        # title
+        recipe += "= " + recipe_dict["title"] + " =" + "\n"
+
+        # ingredients
+        def ingredient_to_string(_ingredient: dict):
+            """Combines amount, unit and name to one string.
+
+            If amount is empty or 0, amount and unit are ignored."""
+
+            ingredient_string = ""
+
+            # add amount and unit if necessary
+            if ingredient["amount"] and ingredient["amount"] != 0:
+                ingredient_string += (
+                    str(ingredient["amount"]) + " " + ingredient["unit"] + " "
+                )
+
+            # add name of ingredient
+            ingredient_string += ingredient["name"]
+
+            return ingredient_string
+
+        recipe += "\n"
+        for group in recipe_dict["text"]["ingredientGroups"]:
+            if not group["header"].strip() == "":
+                recipe += "= = " + group["header"] + " ==\n"
+            for ingredient in group["ingredients"]:
+                recipe += f"= = = {ingredient_to_string(ingredient)} = = =\n"
+        recipe += "\n"
+
+        # instructions
+        recipe += "Instructions:\n"
+        recipe += recipe_dict["text"]["instructions"] + "\n"
+        return recipe
+
+    def get_formatted_text(self):
+        formatted_text = f"\n".join(map(self._reicpe_to_text, self.data))
+        return formatted_text
