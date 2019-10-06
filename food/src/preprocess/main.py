591b7dff876d25ac3f091a9694fbf0b7a7546cd3
Sebastian Fellner <fellnerseb@gmail.com>
2019-10-06 16:07:41 +0200

    minor changes
    export preliminary text.txt

--- food/src/preprocess/main.py
+++ food/src/preprocess/main.py
@@ -2,15 +2,18 @@ from preprocess.data_handling import DataHandler
 from preprocess.exporting import Exporter
 
 
-def do_filtering():
+def do_filtering(data=None):
     d = DataHandler()
     return d.get_filtered_data(
-        [lambda x: x["rating"], lambda x: x["rating"]["numVotes"] >= 10],
-        list(d.read_small_data()),
+        [lambda x: x["rating"], lambda x: x["rating"]["numVotes"] >= 10], data
     )
 
 
+def export_data(data):
+    exporter = Exporter(data)
+    with open("text.txt", "w") as f:
+        f.write(exporter.get_formatted_text())
+
+
 if __name__ == "__main__":
-    filtered_data = do_filtering()
-    exporter = Exporter(filtered_data)
-    print(exporter.get_formatted_text())
+    export_data(do_filtering())
