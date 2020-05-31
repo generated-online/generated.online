258d318cfe8444b6c644bf368dd0286ba6508d59
Moritz Wolf <moritz.m.wolf@tum.de>
2020-05-31 09:26:35 +0200

    add search bar

--- frontend/recepies.generated.online/src/plugins/vuetify.js
+++ frontend/recepies.generated.online/src/plugins/vuetify.js
@@ -13,9 +13,16 @@ export default new Vuetify({
     themes: {
       light: {
         primary: '#3f51b5',
-            secondary: '#b0bec5',
+            secondary: '#f7f6f9',
             accent: '#8c9eff',
             error: '#b71c1c',
+          //
+              GOdark: "#323E40",
+              GOgrey: "#939ca8",
+              GOyellow: "#F2AB27",
+              GOorange: "#D97D0D",
+              GOdarkred: "#732002",
+              GOred: "#D94D1A",
       },
     },
   },
