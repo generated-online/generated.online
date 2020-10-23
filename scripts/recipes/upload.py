a48092cd41da0880a6734ff85618d34fdd5e0253
Moritz Wolf <moritz.m.wolf@tum.de>
2020-10-23 17:03:16 +0200

    add upload.py

--- scripts/recipes/upload.py
+++ scripts/recipes/upload.py
@@ -4,13 +4,15 @@ from firebase_admin import firestore
 import re
 
 # Use service account
-cred = credentials.Certificate("generatedonline-a1cb0-firebase-adminsdk-o3l86-5432fd6352.json")
+cred = credentials.Certificate("generatedonline-a1cb0-firebase-adminsdk-ffohx-b8d5abe895.json")
 firebase_admin.initialize_app(cred)
 # define database
 db = firestore.client()
 
-with open('../test_data.txt') as f:
+with open('recipes/1_200.txt') as f:
     content = f.read()
+
+content = content.replace("<|endoftext|>", "")
 
 results = re.findall("={3}\s(.*)={3}\s*(=\s(([^=])*)\s=)*\s*(([=\s]{6}.*)*)\s*(.*):\s*(([^===])*)", content)
 
@@ -23,5 +25,5 @@ for result in results:
             u'instructions': result[7]
         }        
         db.collection(u'firstTest').document().set(data)
-        print(result[7])
-        print(f"Added: {data['title']}")
+        # print(data)
+        print(f"Added: {data['title']}\n")
