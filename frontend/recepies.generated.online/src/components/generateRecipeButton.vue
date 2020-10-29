f13dfade2d519f8ea03027834e1bb8ea12d318ad
Sebastian Fellner <fellnerseb@gmail.com>
2020-10-29 09:59:10 +0100

    rename reload button

--- frontend/recepies.generated.online/src/components/generateRecipeButton.vue
+++ frontend/recepies.generated.online/src/components/generateRecipeButton.vue
@@ -3,7 +3,7 @@
     color: rgb(100, 100, 100);
     font-weight: 600;">
         <span class="mx-3 mr-0">
-            Neues Rezept
+            Zufälliges Rezept
         </span>
         <v-btn class="mx-3 ml-0" icon>
             <img src="/reload.svg" class="my-icon" />
