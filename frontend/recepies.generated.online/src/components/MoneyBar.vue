c5f8f0de2688fc8cf3e622a8c17b0e14a7137921
Sebastian Fellner <fellnerseb@gmail.com>
2020-12-01 11:07:53 +0100

    extract MoneyBar.vue from PayPalPostcard.vue input fields ind PayPalPostcard.vue are black now
    format Recipe.vue

---
+++ frontend/recepies.generated.online/src/components/MoneyBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div
+            style="width: 100%; margin-top: 1em; height: 100%; padding: 11px; vertical-align: center; text-shadow: none !important">
+        <div
+                style="width: 100%; height: 1.5em; border-radius: 5px; overflow: hidden; box-shadow: 0 0 10px black; background: rgba(1,1,1,0.8); color: white;">
+            <div class="progress-item" :style="'width:' +  100* 2.20 / (price + money) + '%'">
+                <b> 🖨 Druck
+                </b>
+            </div>
+            <div class="progress-item" :style="'width:' +  100*0.70 / (price + money) + '%'">
+                <b> 📪 Versand
+                </b>
+            </div>
+            <div class="progress-item" :style="'width:' +  100* 0.35 / (price + money) + '%'">
+                <b> 🏦 Paypal
+                </b>
+            </div>
+            <div class="progress-item" :style="'width:' + 100* 0.25 / (price + money) + '%'">
+                <b> 💻 Etc.
+                </b>
+            </div>
+            <div v-show="money > 0" class="progress-item" style="border: none !important"
+                 :style="'width:' + 100* money / (price + money) + '%'">
+                <b> 🤩 Spenden
+                </b>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'MoneyBar',
+    props: {
+        money: {},
+        price: {}
+    }
+}
+</script>
+<style scoped>
+.progress-item {
+    min-width: 1.4em;
+    border-right: 1px solid white;
+    padding-left: 0.5em;
+    display: inline-flex;
+}
+</style>
--- frontend/recepies.generated.online/src/views/Recipe.vue
+++ frontend/recepies.generated.online/src/views/Recipe.vue
@@ -1,258 +1,254 @@
 <template>
-  <div class="recipe-container">
-    <div v-if='recipe'>
-      <EmojieBackground :recipe="recipe" emojiContainerSize='7em' :opacity="0.3" />
-      <div style="min-height: 100vh">
-        <div class="title-container">
-          <h1 class="recipe-title text-span dynamic-font-size">
-            {{ recipe.title }}
-          </h1>
-          <Voting class="recipe-vote dynamic-font-size text-span" :recipe='recipe' />
-        </div>
+    <div class="recipe-container">
+        <div v-if='recipe'>
+            <EmojieBackground :recipe="recipe" emojiContainerSize='7em' :opacity="0.3"/>
+            <div style="min-height: 100vh">
+                <div class="title-container">
+                    <h1 class="recipe-title text-span dynamic-font-size">
+                        {{ recipe.title }}
+                    </h1>
+                    <Voting class="recipe-vote dynamic-font-size text-span" :recipe='recipe'/>
+                </div>
 
-        <div class="recipe-body">
-          <!-- ZUTATEN -->
-          <div class="ingredients text-span">
-            <div class="ingredient" :key="ingredient+String(Math.floor(Math.random() * 100))" justify="center"
-              v-for="ingredient in recipe.ingredients">
-              <span>{{ ingredient }}</span>
-            </div>
-            <div class="divider"></div>
-          </div>
-
-          <!--  Instructions -->
-          <span class="instruction text-span">
+                <div class="recipe-body">
+                    <!-- ZUTATEN -->
+                    <div class="ingredients text-span">
+                        <div class="ingredient" :key="ingredient+String(Math.floor(Math.random() * 100))"
+                             v-for="ingredient in recipe.ingredients">
+                            <span>{{ ingredient }}</span>
+                        </div>
+                        <div class="divider"></div>
+                    </div>
+                    <!--  Instructions -->
+                    <span class="instruction text-span">
             {{ recipe.instructions }}
           </span>
+                </div>
+                <!--  Postcard -->
+                <PayPalPostcard :recipe='recipe'/>
+            </div>
         </div>
-
-        <!--  Postcard -->
-        <PayPalPostcard :recipe='recipe' />
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-  import firebase from "firebase";
-  import recipeToColor from "@/functions/recipe_to_color";
-  import Voting from "@/components/Voting"
-  import EmojieBackground from "@/components/EmojieBackground"
-  import PayPalPostcard from "@/components/PayPalPostcard"
+import firebase from "firebase";
+import Voting from "@/components/Voting"
+import EmojieBackground from "@/components/EmojieBackground"
+import PayPalPostcard from "@/components/PayPalPostcard"
 
-  export default {
+export default {
     name: "recipe",
     components: {
-      Voting,
-      EmojieBackground,
-      PayPalPostcard
+        Voting,
+        EmojieBackground,
+        PayPalPostcard
     },
     data() {
-      return {
-        id: "",
-        recipe: undefined,
-        error: "",
-      };
+        return {
+            id: "",
+            recipe: undefined,
+            error: "",
+        };
     },
     created() {
-      let db = firebase.firestore();
-      const ref = db.collection("recipes")
+        let db = firebase.firestore();
+        const ref = db.collection("recipes")
 
-      this.id = this.$route.params.id;
-      let key = "";
-      if (typeof this.id !== "undefined") {
-        ref
-          .doc(this.id)
-          .get()
-          .then((doc) => {
-            this.loadData(doc);
-          });
-      } else {
-        key = ref.doc().id;
-        ref
-          .where(firebase.firestore.FieldPath.documentId(), ">=", key)
-          .limit(1)
-          .get()
-          .then((snap) => {
-            if (snap.size === 0) {
-              ref
-                .where(firebase.firestore.FieldPath.documentId(), "<", key)
+        this.id = this.$route.params.id;
+        let key = "";
+        if (typeof this.id !== "undefined") {
+            ref
+                .doc(this.id)
+                .get()
+                .then((doc) => {
+                    this.loadData(doc);
+                });
+        } else {
+            key = ref.doc().id;
+            ref
+                .where(firebase.firestore.FieldPath.documentId(), ">=", key)
                 .limit(1)
                 .get()
                 .then((snap) => {
-                  snap.docs.map(this.loadData);
+                    if (snap.size === 0) {
+                        ref
+                            .where(firebase.firestore.FieldPath.documentId(), "<", key)
+                            .limit(1)
+                            .get()
+                            .then((snap) => {
+                                snap.docs.map(this.loadData);
+                            });
+                    } else {
+                        snap.docs.map(this.loadData);
+                    }
+                })
+                .catch((err) => {
+                    this.error = err;
                 });
-            } else {
-              snap.docs.map(this.loadData);
-            }
-          })
-          .catch((err) => {
-            this.error = err;
-          });
-      }
+        }
     },
     methods: {
-      loadData(doc) {
-        this.recipe = {
-          id: doc.id,
-          ingredients: doc.data().ingredients,
-          title: doc.data().title,
-          instructions: doc.data().instructions,
-          votes: doc.data().votes || 0,
-        };
+        loadData(doc) {
+            this.recipe = {
+                id: doc.id,
+                ingredients: doc.data().ingredients,
+                title: doc.data().title,
+                instructions: doc.data().instructions,
+                votes: doc.data().votes || 0,
+            };
 
-        this.$emit('shareText', 'Schau dir dieses coole KI generierte Rezept an: ' + this.recipe['title']);
-        this.$emit('recipeId', this.recipe.id);
-        if (this.id === undefined) {
-          this.$router.replace("/recipe/" + doc.id);
-        }
-      },
+            this.$emit('shareText', 'Schau dir dieses coole KI generierte Rezept an: ' + this.recipe['title']);
+            this.$emit('recipeId', this.recipe.id);
+            if (this.id === undefined) {
+                this.$router.replace("/recipe/" + doc.id);
+            }
+        },
     }
-  };
+};
 </script>
 
 <style scoped>
-  .recipe-container {
+.recipe-container {
     text-align: left;
     height: 100%;
     padding: 2em 2em 2em 2em;
     margin-bottom: 2em;
-  }
+}
 
-  .title-container {
+.title-container {
     width: 100%;
     overflow: hidden;
     margin-bottom: 1.5em;
     display: flex;
-  }
+}
 
-  .dynamic-font-size {
+.dynamic-font-size {
     font-size: calc(70vw / 15);
-    font-family: "Commissioner";
+    font-family: "Commissioner",serif;
     padding: 0.2em 0.2em 0.2em 0.2em;
-  }
+}
 
-  .recipe-title {
+.recipe-title {
     word-wrap: break-word;
     flex-grow: 1;
     margin-right: 0.5em
-  }
+}
 
-  .recipe-vote {
+.recipe-vote {
     float: right;
     height: fit-content;
-  }
+}
 
-  .recipe-body {
+.recipe-body {
     padding-bottom: 1.5em;
     display: flex;
-  }
+}
 
-  .ingredient {
+.ingredient {
     padding-bottom: 0.5em;
     font-size: 1.2em;
     white-space: nowrap
-  }
+}
 
-  .ingredients {
+.ingredients {
     width: fit-content;
     padding: 2%
-  }
+}
 
-  .instruction {
+.instruction {
     font-size: 1.5em;
     flex-grow: 1;
     text-align: justify;
     display: block;
     padding: 2%;
     margin-left: 2%;
-  }
+}
 
-  @media (max-width: 800px) {
+@media (max-width: 800px) {
     .dynamic-font-size {
-      font-size: calc(70vw / 8);
+        font-size: calc(70vw / 8);
     }
 
     .title-container {
-      text-align: center;
-      display: block;
-      align-items: center;
+        text-align: center;
+        display: block;
+        align-items: center;
     }
 
     .recipe-title {
-      word-wrap: break-word;
-      flex-grow: 0;
-      margin-right: 0;
-      width: 80%;
-      margin: auto;
-      margin-bottom: 0.5em;
+        word-wrap: break-word;
+        flex-grow: 0;
+        width: 80%;
+        margin: auto;
+        margin-bottom: 0.5em;
     }
 
     .recipe-vote {
-      float: unset;
-      width: fit-content;
-      height: fit-content;
-      margin: auto;
+        float: unset;
+        width: fit-content;
+        height: fit-content;
+        margin: auto;
     }
 
     .recipe-body {
-      position: relative;
-      overflow: hidden;
-      display: block;
+        position: relative;
+        overflow: hidden;
+        display: block;
     }
 
     .ingredient {
-      padding-bottom: 0.5em;
-      width: 50%;
-      float: left;
+        padding-bottom: 0.5em;
+        width: 50%;
+        float: left;
     }
 
     .ingredients {
-      width: 100%;
-      margin-bottom: 2em;
-      float: left;
-      position: relative;
+        width: 100%;
+        margin-bottom: 2em;
+        float: left;
+        position: relative;
     }
 
     .instruction {
-      float: left;
-      margin-left: 0;
+        float: left;
+        margin-left: 0;
     }
 
     .divider {
-      position: absolute;
-      left: 48%;
-      top: 5%;
-      bottom: 5%;
-      border-left: 3px solid black;
+        position: absolute;
+        left: 48%;
+        top: 5%;
+        bottom: 5%;
+        border-left: 3px solid black;
     }
-  }
+}
 
-  @media (min-width: 500px) and (max-width: 600px) {
+@media (min-width: 500px) and (max-width: 600px) {
     .ingredient {
-      font-size: 1em;
-      padding-bottom: 0.5em;
-      width: 50%;
-      float: left;
+        font-size: 1em;
+        padding-bottom: 0.5em;
+        width: 50%;
+        float: left;
     }
 
     .divider {
-      border-left: 1.5px solid black
+        border-left: 1.5px solid black
     }
-  }
+}
 
-  @media (max-width: 500px) {
+@media (max-width: 500px) {
     .ingredient {
-      font-size: 1.4em;
-      padding-bottom: 0.5em;
+        font-size: 1.4em;
+        padding-bottom: 0.5em;
 
-      width: 100%;
-      float: none;
+        width: 100%;
+        float: none;
 
     }
 
     .divider {
-      border-left: none;
+        border-left: none;
     }
-  }
+}
 </style>
