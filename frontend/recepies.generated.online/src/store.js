2c386dddf21301c45b0eed4f26bdf2ddd34a3266
Sebastian Fellner <fellnerseb@gmail.com>
2020-12-01 10:36:42 +0100

    make vue plugin work

--- frontend/recepies.generated.online/src/store.js
+++ frontend/recepies.generated.online/src/store.js
@@ -3,14 +3,11 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// vue dev plugin
+Vue.config.devtools = process.env.NODE_ENV === 'development'
+
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    state: {},
+    mutations: {},
+    actions: {}
 })
