1476e1d7a5d4bd46654e2d17a97ae546ef2abde1
Sebastian Fellner <fellnerseb@gmail.com>
2020-12-20 15:10:27 +0100

    refactor Highscore.vue into new RecipeCard.vue

---
+++ frontend/recepies.generated.online/src/components/RecipeCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <router-link :to="'/recipe/' + recipe.id" class="boldyNoColor px-4 py-1 mb-4" style="width:100%">
+        <v-row align="center" cols="12" no-gutters>
+            <v-col align="center" class="text-h3" cols="auto" style="min-width: 1.75em">
+                {{ recipe.votes }}
+            </v-col>
+            <v-col class="ml-2">
+                <v-row class="text-h6">
+                    {{ recipe.title }}
+                </v-row>
+                <v-row class="mb-1 text-h6">
+                    <img v-for="emoji in recipeToEmojis.map(getImgUrl)" :key="emoji" :src="emoji" class="emoji pr-2">
+                </v-row>
+            </v-col>
+        </v-row>
+    </router-link>
+</template>
+<script>
+import {getImgUrl} from "@/functions/emojiUtils"
+
+export default {
+    name: 'RecipeCard',
+    props: {
+        recipe: {},
+        recipeToEmojis: {}
+    },
+    methods: {
+        getImgUrl
+    }
+}
+</script>
+<style scoped>
+a {
+    text-decoration: none;
+    color: inherit !important;
+}
+
+.emoji {
+    vertical-align: middle;
+    height: 1em;
+}
+</style>
--- frontend/recepies.generated.online/src/views/Highscore.vue
+++ frontend/recepies.generated.online/src/views/Highscore.vue
@@ -1,99 +1,83 @@
 <template>
     <v-container>
-        <v-row align="center" justify="center" class="text-h2 boldy mb-8 text-center" style="font-weight:bold;">
-            <img src="../assets/robokoch.png" class="emoji">
+        <v-row align="center" class="text-h2 boldy mb-8 text-center" justify="center" style="font-weight:bold;">
+            <img class="emoji" src="/robokoch.gif">
             Beschde Rezepte
         </v-row>
-        <v-row v-for="recipe in recipes" :style="{'color':(recipeToColor(recipe.id) +' !important')}" class="row" :key="recipe.id"
+        <v-row v-for="recipe in recipes" :key="recipe.id" :style="{'color':(recipeToColor(recipe.id) +' !important')}"
+               class="row"
                no-gutters>
-            <router-link :to="'/recipe/' + recipe.id" class="boldyNoColor px-4 py-1 mb-4" style="width:100%">
-                <v-row cols="12" no-gutters align="center">
-                    <v-col align="center" class="text-h3" cols="auto" style="min-width: 1.75em">
-                        {{ recipe.votes }}
-                    </v-col>
-                    <v-col class="ml-2">
-                        <v-row class="text-h6">
-                            {{ recipe.title }}
-                        </v-row>
-                        <v-row class="mb-1 text-h6">
-                            <img v-for="emoji in recipeToEmojis(recipe).map(getImgUrl)" :src="emoji" class="emoji pr-2" :key="emoji">
-                        </v-row>
-                    </v-col>
-                </v-row>
-            </router-link>
+            <RecipeCard :recipe="recipe" :recipe-to-emojis="recipeToEmojis(recipe)"/>
         </v-row>
     </v-container>
 </template>
 
 <script>
-    import firebase from "firebase";
-    import recipeToColor from "@/functions/recipe_to_color";
-    import {
-        recipeToEmojis,
-        getImgUrl
-    } from "@/functions/emojiUtils"
+import firebase from "firebase";
+import recipeToColor from "@/functions/recipe_to_color";
+import {recipeToEmojis} from "@/functions/emojiUtils"
+import RecipeCard from "@/components/RecipeCard";
 
-    export default {
-        name: "MostViewedRecipes",
-        components: {},
-        props: {
-            "num_recipes": {
-                type: Number,
-                default: 10
-            }
-        },
-        data() {
-            return {
-                recipes: [],
-            };
-        },
-        created() {
-            let db = firebase.firestore();
-            const ref = db.collection("recipes")
-            ref.orderBy("votes", "desc").limit(this.num_recipes)
-                .get()
-                .then((snap) => {
-                    if (snap.size === 0) {
-                        console.log("No highscores found")
-                    } else {
-                        snap.docs.map(this.loadData);
-                    }
-                }).catch((err) => {
-                    console.error(err)
-                });
-            this.$emit('shareText', "Schau dir diese bestbewerteten von einer KI generierten Rezepte an!");
-            this.$emit('recipe', null);
-        },
-        methods: {
-            loadData(doc) {
-                this.recipes.push({
-                    id: doc.id,
-                    ingredients: doc.data().ingredients,
-                    title: doc.data().title,
-                    instructions: doc.data().instructions,
-                    votes: doc.data().votes || 0,
-                });
-            },
-            recipeToColor,
-            recipeToEmojis,
-            getImgUrl
+export default {
+    name: "MostViewedRecipes",
+    components: {RecipeCard},
+    props: {
+        "num_recipes": {
+            type: Number,
+            default: 10
         }
+    },
+    data() {
+        return {
+            recipes: [],
+        };
+    },
+    created() {
+        let db = firebase.firestore();
+        const ref = db.collection("recipes")
+        ref.orderBy("votes", "desc").limit(this.num_recipes)
+            .get()
+            .then((snap) => {
+                if (snap.size === 0) {
+                    console.log("No highscores found")
+                } else {
+                    snap.docs.map(this.loadData);
+                }
+            }).catch((err) => {
+            console.error(err)
+        });
+        this.$emit('shareText', "Schau dir diese bestbewerteten von einer KI generierten Rezepte an!");
+        this.$emit('recipe', null);
+    },
+    methods: {
+        loadData(doc) {
+            this.recipes.push({
+                id: doc.id,
+                ingredients: doc.data().ingredients,
+                title: doc.data().title,
+                instructions: doc.data().instructions,
+                votes: doc.data().votes || 0,
+            });
+        },
+        recipeToColor,
+        recipeToEmojis,
     }
+}
 </script>
 
 <style scoped>
-    a {
-        text-decoration: none;
-        color: inherit !important;
-    }
+a {
+    text-decoration: none;
+    color: inherit !important;
+}
 
-    .row {
-        font-weight: bold;
+.row {
+    font-weight: bold;
 
-    }
+}
 
-    .emoji {
-        vertical-align: middle;
-        height: 1em;
-    }
+.emoji {
+    vertical-align: middle;
+    height: 1em;
+}
 </style>
