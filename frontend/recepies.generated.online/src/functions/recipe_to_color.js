ee448a806563e7a05db886dd331e5e8260751f8e
Sebastian Fellner <fellnerseb@gmail.com>
2020-12-23 10:03:02 +0100

    better hash conversion

--- frontend/recepies.generated.online/src/functions/recipe_to_color.js
+++ frontend/recepies.generated.online/src/functions/recipe_to_color.js
@@ -1,3 +1,15 @@
+function stringToIntHash(str, upperbound, lowerbound) {
+    let result = 0;
+    for (let i = 0; i < str.length; i++) {
+        result = result + str.charCodeAt(i);
+    }
+
+    if (!lowerbound) lowerbound = 0;
+    if (!upperbound) upperbound = 500;
+
+    return (result % (upperbound - lowerbound)) + lowerbound;
+}
+
 export default (id) => {
     const colors = [
         "lightcoral",
@@ -12,7 +24,13 @@ export default (id) => {
         "lightyellow",
         "lightseagreen",
     ]
-    // lightblue is default color
-    let num = (parseInt(id, 20) || 2) % colors.length;
+    let num;
+    if (id === null){
+        // lightblue is default color
+        num = 2
+    }
+    else {
+        num = stringToIntHash(id, colors.length, 0);
+    }
     return colors[num];
 };
